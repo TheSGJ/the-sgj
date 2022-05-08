27871acb8208ebf8902f7ab889b03d91ffa73c90
TheSGJ <60005430+TheSGJ@users.noreply.github.com>
2022-05-09 03:08:14 +0530

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require('gray-matter')
 async function generate() {
   const feed = new RSS({
     title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    site_url: 'https://thesgj.vercel.app/',
+    feed_url: 'https://thesgj.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
