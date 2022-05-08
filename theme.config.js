cc0be08cecf73140ecb22d35bc9cdde37d91b30c
TheSGJ <60005430+TheSGJ@users.noreply.github.com>
2022-05-08 23:07:59 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,12 +3,8 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
+      <time>{YEAR}</time> © TheSGJ.
+      
         @media screen and (max-width: 480px) {
           article {
             padding-top: 2rem;
