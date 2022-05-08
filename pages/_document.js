894d9722b764361221ff6c945a56860848d37485
TheSGJ <60005430+TheSGJ@users.noreply.github.com>
2022-05-09 01:35:07 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
+    title: 'TheSGJ',
     description: 'Clone and deploy your own Next.js portfolio in minutes.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
