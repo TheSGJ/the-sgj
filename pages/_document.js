608ca16d0430d8dfc32906a4904f13d931163c2b
Shri Ganesh Jha <realsgj@gmail.com>
2022-09-12 13:28:09 +0000

    fixed the files

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   const meta = {
     title: 'TheSGJ',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    description: 'Portfolio & Blog website of TheSGJ',
+    image: 'https://avatars.githubusercontent.com/u/60005430?v=4'
   }
 
   return (
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@TheSGJyt" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
